c8d6e417efaaece90a6ce9d9efea5cd46298cba2
pocorall <pocorall@gmail.com>
2012-10-16 21:10:50 +0900

    copyright header added

--- common.scala
+++ common.scala
@@ -1,3 +1,33 @@
+/*
+ *
+ *
+ *
+ *
+ * Less painful Android development with Scala
+ *
+ * https://github.com/pocorall/android-scala-common
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * Copyright 2012 Sung-Ho Lee
+ *
+ * Sung-Ho Lee licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+
 package net.pocorall.android
 
 import android.app._
@@ -256,7 +286,8 @@ package object common {
 
   trait FollowParentBackButton extends Activity {
     override def onBackPressed() {
-      getParent.onBackPressed()
+      val p = getParent
+      if (p != null) p.onBackPressed()
     }
   }
 
