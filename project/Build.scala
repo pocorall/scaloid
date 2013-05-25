9d07e71fec82257bfb1e1ad0fb627044d8d0eccd
Jisoo Park <xxxyel@gmail.com>
2013-05-25 11:56:44 +0900

    Remove unused libraries

--- project/Build.scala
+++ project/Build.scala
@@ -32,9 +32,7 @@ object Dependencies {
   def runtime   (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "runtime")
   def container (deps: ModuleID*): Seq[ModuleID] = deps map (_ % "container")
 
-  val android = "com.google.android" % "android" % "2.2.1" withSources() withJavadoc()
-  val reflections = "org.reflections" % "reflections" % "0.9.9-RC1"
-  val slf4 = "org.slf4j" % "slf4j-nop" % "1.7.5" // for reflections
+  val android = "com.google.android" % "android" % "2.2.1"
 }
 
 object ScaloidBuild extends Build {
@@ -52,7 +50,7 @@ object ScaloidBuild extends Build {
     .settings(name := "scaloid", exportJars := true)
     .settings(basicSettings: _*)
     .settings(scaloidSettings: _*)
-    .settings(libraryDependencies ++= Seq(reflections, slf4) ++ provided(android))
+    .settings(libraryDependencies ++= provided(android))
 
 }
 
