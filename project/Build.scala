23caabcca5ddf8dfa758dedb64dbd0b4eaae6315
Sung-Ho Lee <pocorall@gmail.com>
2013-06-07 00:34:20 +0900

    added metadata for publishing

--- project/Build.scala
+++ project/Build.scala
@@ -13,6 +13,36 @@ object ScaloidBuild extends Build {
     startYear             := Some(2012),
     scalaVersion          := "2.10.1",
     resolvers             ++= Dependencies.resolutionRepos,
+    publishMavenStyle := true,
+    publishTo <<= version { (v: String) =>
+      val nexus = "https://oss.sonatype.org/"
+      if (v.trim.endsWith("SNAPSHOT"))
+        Some("snapshots" at nexus + "content/repositories/snapshots")
+      else
+        Some("releases"  at nexus + "service/local/staging/deploy/maven2")
+    },
+    pomIncludeRepository := { _ => false },
+    pomExtra := (
+      <url>http://jsuereth.com/scala-arm</url>
+      <licenses>
+        <license>
+          <name>The Apache Software License, Version 2.0</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:pocorall/scaloid.git</url>
+        <connection>scm:git:git@github.com:pocorall/scaloid.git</connection>
+		<developerConnection>scm:git:git@github.com:pocorall/scaloid.git</developerConnection>
+      </scm>
+      <developers>
+        <developer>
+          <id>pocorall</id>
+          <name>Sung-Ho Lee</name>
+          <email>pocorall@gmail.com</email>
+        </developer>
+      </developers>),
     scalacOptions         := Seq(
       "-target:jvm-1.6", "-deprecation", "-feature"
     ),
@@ -24,7 +54,6 @@ object ScaloidBuild extends Build {
   override lazy val settings = super.settings :+ {
     shellPrompt := { s => Project.extract(s).currentProject.id + "> " }
   }
-
 
   // root project
 //  lazy val parent = Project("parent", file("."))
