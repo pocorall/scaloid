b71ea100628c0b5ac8b9fe7661ac86811a3d8ec7
Sung-Ho Lee <pocorall@gmail.com>
2013-06-11 03:17:11 +0900

    added apidoc

--- scaloid-common/src/main/st/org/scaloid/common/package.scala
+++ scaloid-common/src/main/st/org/scaloid/common/package.scala
@@ -9,9 +9,37 @@ import annotation.target.{beanGetter, getter}
 import language.implicitConversions
 
 /**
- * Common Scaloid Package
+ * Scaloid marries Android code with Scala resulting in easier to understand
+ * and maintain code.
  *
- * @see [[org.scaloid.TermsAndConditions]]
+ *
+ * @example
+ *
+ * {{{
+ * import org.scaloid.common._
+ *
+ * class MainActivity extends SActivity {
+ *
+ *   onCreate {
+ *     contentView = new SVerticalLayout {
+ *       setTheme(android.R.style.Theme_Holo_NoActionBar)
+ *
+ *       style {
+ *         case b:SButton => b.textSize(22 dip)
+ *       }
+ *
+ *       STextView("Welcome").textSize(22 sp).\<\<.marginBottom(22 dip).>>
+ *
+ *       val name = SEditText()
+ *       STextView("What is your name?").\<\<.marginBottom(22 dip).>>
+ *
+ *       SButton("GO").onClick(longToast(s"Hello, \${name.getText}"))
+ *     }.padding(20 dip)
+ *   }
+ * }
+ * }}}
+ *
+ * @see [[http://scaloid.org]]
  *
  * @author Sung-Ho Lee
  */
