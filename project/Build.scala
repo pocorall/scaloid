497d93074cae668e8574a73484df93edc9fa10b4
Sung-Ho Lee <pocorall@gmail.com>
2014-04-20 01:13:05 +0900

    start building Scaloid 3.4

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   val android = "com.google.android" % "android" % "2.2.1" % "provided"
   val android_support_v4 = "com.google.android" % "support-v4" % "r7" % "provided"
-  val scaloidVersion = "3.3-8"
+  val scaloidVersion = "3.4-8-SNAPSHOT"
   val scaloid = "org.scaloid" %% "scaloid" % scaloidVersion
 }
 
@@ -27,7 +27,7 @@ object ScaloidBuild extends Build {
     organizationHomepage := Some(new URL("http://blog.scaloid.org")),
     description := "Less Painful Android Development with Scala",
     startYear := Some(2012),
-    scalaVersion := "2.10.3",
+    scalaVersion := "2.11.0",
     version := scaloidVersion,
     resolvers ++= Dependencies.resolutionRepos,
     publishMavenStyle := true,
