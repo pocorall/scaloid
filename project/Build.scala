929af1093b6e68e8443adba63c5f020f37d1c61a
Sung-Ho Lee <pocorall@gmail.com>
2014-04-19 23:00:26 +0900

    released Scaloid 3.3

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   val android = "com.google.android" % "android" % "2.2.1" % "provided"
   val android_support_v4 = "com.google.android" % "support-v4" % "r7" % "provided"
-  val scaloidVersion = "3.3-8-SNAPSHOT"
+  val scaloidVersion = "3.3-8"
   val scaloid = "org.scaloid" %% "scaloid" % scaloidVersion
 }
 
