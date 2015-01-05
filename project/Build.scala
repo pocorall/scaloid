e358ecb94e19798a55299fd434f6fb87c123cb39
Sung-Ho Lee <pocorall@gmail.com>
2015-01-05 17:54:08 +0900

    fix #113

--- project/Build.scala
+++ project/Build.scala
@@ -27,7 +27,7 @@ object ScaloidBuild extends Build {
     description := "Less Painful Android Development with Scala",
     startYear := Some(2012),
     scalaVersion := "2.11.4",
-    crossScalaVersions := Seq("2.10.4", "2.11.4"),
+    crossScalaVersions := Seq("2.11.4"), // only 2.11.x for now
     version := scaloidVersion,
     publishMavenStyle := true,
     publishTo <<= version {
